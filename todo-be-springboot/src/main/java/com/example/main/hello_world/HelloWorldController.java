6cc591ffb75784ba2eb02e3354e5d7d5f9ca18c0
Devesh <devucp021@gmail.com>
2024-05-12 19:40:02 +0530

    jwt

--- todo-be-springboot/src/main/java/com/example/main/hello_world/HelloWorldController.java
+++ todo-be-springboot/src/main/java/com/example/main/hello_world/HelloWorldController.java
@@ -5,10 +5,12 @@ package com.example.main.hello_world;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200/")
-@RestController("/welcome")
+@RestController()
+@RequestMapping(path = "/welcome")
 public class HelloWorldController {
 	
 	
